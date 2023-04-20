50dd3c4cfa74671bf59a0f7830acef6b80634d98
Benjamin Jackson <benjacksonk@gmail.com>
2023-04-20 15:40:25 -0400

    created oregon trail and added the concept of a traveler

---
+++ src/main/java/org/yup/oregontrail/OregonTrailApp.java
@@ -0,0 +1,21 @@
+package org.yup.oregontrail;
+
+public class OregonTrailApp {
+
+    public static void main(String[] args) {
+
+        Traveler dwene = new Traveler("Dwene", 100, 100);
+
+        System.out.println("before hunting food: " + dwene.getFood());
+        System.out.println(dwene.hunt());
+        System.out.println("after hunting food: " + dwene.getFood());
+        System.out.println(dwene.hunt());
+        System.out.println(dwene.eat());
+        System.out.println(dwene.eat());
+        System.out.println(dwene.eat());
+        System.out.println(dwene.eat());
+        System.out.println(dwene.getFood());
+
+    }
+gi
+}
